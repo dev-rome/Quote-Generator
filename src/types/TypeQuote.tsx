a3b878e04d9b24f909887e011c304ee9fe3fdadf
dev-rome <info.devrome@gmail.com>
2023-01-30 21:41:01 -0500

    created interface for quote to be exported to quote.tsx

---
+++ src/types/TypeQuote.tsx
@@ -0,0 +1,8 @@
+export interface Quotes {
+  quoteText: string;
+  quoteAuthor: string;
+  buttonText: string;
+  iconTwitter: any;
+  onClickTwitter?: React.MouseEventHandler;
+  onClickQuote?: React.MouseEventHandler;
+}
