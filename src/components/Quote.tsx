fce3ce8e096a3d196ec58e0829dd9b98ce619e0a
dev-rome <info.devrome@gmail.com>
2023-01-30 21:51:51 -0500

    created quote component to store quote and pass props using quotes interface

---
+++ src/components/Quote.tsx
@@ -0,0 +1,26 @@
+import { ButtonContainer, QuoteContainer, QuoteWrapper } from "../styles/QuoteStyles";
+import { Quotes } from "../types/TypeQuote";
+
+const Quote = ({
+  quoteText,
+  quoteAuthor,
+  buttonText,
+  iconTwitter,
+  onClickTwitter,
+  onClickQuote,
+}: Quotes) => {
+  return (
+    <QuoteContainer>
+      <QuoteWrapper>
+       <p><q>{quoteText}</q></p>
+        <p>{quoteAuthor}</p>
+        <ButtonContainer>
+          <button onClick={onClickTwitter}>{iconTwitter}</button>
+          <button onClick={onClickQuote}>{buttonText}</button>
+        </ButtonContainer>
+      </QuoteWrapper>
+    </QuoteContainer>
+  );
+};
+
+export default Quote;
